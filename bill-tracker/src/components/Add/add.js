1604d76bfae17e1e8f256021b901d4eef0764605
Chris Jonathan <christophereko@hotmail.fr>
2019-01-10 18:54:03 +0000

    I've started working on the Add page, I've added a form, some input elements and a submit button.

--- bill-tracker/src/components/Add/add.js
+++ bill-tracker/src/components/Add/add.js
@@ -9,7 +9,20 @@ class Add extends Component {
     render() {
         return (
             <div>
-                <h1>Add Bills!</h1>
+                <section id="add">
+                    <h1>Add Bills!</h1>
+                </section>
+                <section id="form-holder">
+                    <form method ="POST" action="http://localhost:5000/add" enctype="multipart/form-data">
+                        <label for="title" class="label">Enter a title...</label>
+                        <input type="text" name="title" class="fields" placeholder="Please enter a title..." autocomplete="off" required/>
+                        <br/>
+                        <label for="amount" class="label">Enter an amount...</label>
+                        <input type="text" name="amount" class="fields" placeholder="Please enter an amount..." autocomplete="off" required/>
+                        <br/>
+                        <input type="submit" value="Submit" id="button"></input>
+                    </form>
+                </section>
             </div>
         );
     }
