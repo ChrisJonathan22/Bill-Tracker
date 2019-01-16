1e0a88013c957ff8b8dc569a5fc39aedd908cb63
Chris Jonathan <christophereko@hotmail.fr>
2019-01-16 09:09:29 +0000

    I have created a table which is using bootstrap for its styling and the data is displayed within table as intended. The project is close to completion though I might add one or two things.

--- bill-tracker/src/components/Bills/bills.js
+++ bill-tracker/src/components/Bills/bills.js
@@ -19,11 +19,23 @@ class Bills extends Component {
         return (
             <div>
                 <h1 id="bills-title">View Bills!</h1>
-                {
-                    this.state.bills.map((bill) => {
-                    return <li key={bill._id}>{bill.title}</li>;
-                })
-                }
+                <table className="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Type</th>
+                            <th scope="col">Amount &#163;</th>
+                            <th scope="col">Date <i class="fas fa-calendar-alt"></i></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+            
+                            {
+                        this.state.bills.map((bill) => {
+                            return <tr className="success"><td>{bill.title}</td><td>{bill.amount}</td><td>{bill.date}</td></tr>;
+                        })
+                            }
+                    </tbody>
+                </table>
             </div>
         );
     }
