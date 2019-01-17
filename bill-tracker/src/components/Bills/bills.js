28ead74cfc254c62a5742268050a8ffe61ceae00
Chris Jonathan <christophereko@hotmail.fr>
2019-01-17 08:22:32 +0000

    I'm working on displaying the data in a chart using chart.js, I've added the chart but there's an error at the moment which I will fix in due time.

--- bill-tracker/src/components/Bills/bills.js
+++ bill-tracker/src/components/Bills/bills.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Chart } from 'chart.js';
 import './bills.css';
 
 class Bills extends Component {
@@ -16,6 +17,44 @@ class Bills extends Component {
     }
 
     render() {
+
+        let ctx = document.getElementById("myChart").getContext('2d');
+                let myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                        datasets: [{
+                            label: '# of Votes',
+                            data: [12, 19, 3, 5, 2, 3],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero:true
+                                }
+                            }]
+                        }
+                    }
+                });
         return (
             <div>
                 <h1 id="bills-title">View Bills!</h1>
@@ -36,6 +75,11 @@ class Bills extends Component {
                             }
                     </tbody>
                 </table>
+                <section id="section">
+                    <div id = "canvas-container">
+                        <canvas id="myChart" width="400" height="400" aria-label = "temperature chart" role = "chart"></canvas>
+                    </div>
+                </section>
             </div>
         );
     }
