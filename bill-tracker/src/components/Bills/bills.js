969fc8b7d972f3824620173c3a93a44c963038b1
Chris Jonathan <christophereko@hotmail.fr>
2019-01-20 11:07:19 +0000

    I've added more comments and removed most things that I no longer require, I've given the pound & calendar icons the color green to make them stand out more but I still have one or two things that I need to do.

--- bill-tracker/src/components/Bills/bills.js
+++ bill-tracker/src/components/Bills/bills.js
@@ -5,34 +5,22 @@ import './bills.css';
 class Bills extends Component {
     constructor() {
         super();
-        this.state = {
+        this.state = {  // Set the state and give it some properties.
             bills: [],
             thisYear: [],
             lastYear: [],
         }
-        // this.getDate = this.getDate.bind(this);
     }
 
-    // Create a method.
-    // getDate() {
-        
-    //     setTimeout(() => {
-    //         this.state.bills.map((bill) => {
-    //             console.log(bill.date);
-    //             console.log('It works.');
-    //         });
-    //     }, 5000);
-    // }
-
-    componentDidMount() {
-        let date = new Date();
-        let year = date.getFullYear();
-        let thisYearReg = new RegExp(`${year}`, "g");
-        let lastYearReg = new RegExp(`${year - 1}`, "g");
-        let currentYear;
-        let lastYear;
-        let currentYearList = [];
-        let lastYearList = [];
+    componentDidMount() { // If component mounted do this.
+        let date = new Date(); // Create a new date object.
+        let year = date.getFullYear(); // Get the year.
+        let thisYearReg = new RegExp(`${year}`, "g"); // Create a regex with the current year.
+        let lastYearReg = new RegExp(`${year - 1}`, "g"); // Create a regex with the previous year.
+        let currentYear; // Create a variable where each current year will be initially saved.
+        let lastYear; // Create a variable where each previous year will be initially saved.
+        let currentYearList = []; // Create an empty array where all the current year will be stored.
+        let lastYearList = []; // Create an empty array where all the previous year will be stored.
         
         
         // Fetch all the bills.
@@ -41,16 +29,16 @@ class Bills extends Component {
         // Give the state some new data (bills objects) and store it within an array called bills & log the array.
         .then(data => this.setState({ bills: data.bills }, () => console.log(this.state.bills)));
 
-        // When the component mounts create the chart.
-        setTimeout(() => {
-            let ctx = document.getElementById("myChart").getContext('2d');
-            let myChart = new Chart(ctx, {
+        // When the component mounts create the graph.
+        setTimeout(() => {  // Wait 2 seconds before creating and populating the graph.
+            let ctx = document.getElementById("myGraph").getContext('2d');
+            let myGraph = new Chart(ctx, {
                         type: 'bar',
                         data: {
                             labels: ["Last year", "This year"],
                             datasets: [{
                                 label: 'Bills',
-                                data: [this.state.lastYear.length, this.state.thisYear.length],
+                                data: [this.state.lastYear.length, this.state.thisYear.length], // Use the lastYear and thisYear array length to represent how many bills fit in each year.
                                 backgroundColor: [
                                     'rgba(255, 99, 132, 0.2)',
                                     'rgba(54, 162, 235, 0.2)'
@@ -69,32 +57,35 @@ class Bills extends Component {
                                         beginAtZero:true
                                     }
                                 }]
+                            },
+                            title: {
+                                display: true,
+                                text: "Bills graph overview",
+                                fontColor: "green",
+                                fontSize: 20
                             }
                         }
                     });
         }, 2000);
        
-        setTimeout(() => {
-                    this.state.bills.map((bill) => {
-                        if(bill.date.match(thisYearReg) == year) {
-                            currentYear = bill.date.match(thisYearReg);
-                            currentYearList.push(currentYear);
+        setTimeout(() => { // After 1 second do this.
+                    this.state.bills.map((bill) => { // Map through the array of bills.
+                        if(bill.date.match(thisYearReg) == year) { // Find out if there's a match for the current year with the list of bills. 
+                            currentYear = bill.date.match(thisYearReg); // If there's a match, store it.
+                            currentYearList.push(currentYear); // Push all the matched currentYear into the currentYearList array.
                             console.log("Current year found!");
                         }
-                        else if (bill.date.match(lastYearReg) == year - 1) {
-                            lastYear = bill.date.match(lastYearReg);
-                            lastYearList.push(lastYear);
-                            console.log("Current year not found");
+                        else if (bill.date.match(lastYearReg) == year - 1) { // Find out if there's a match for the previous year within the list of bills.
+                            lastYear = bill.date.match(lastYearReg); // If there's a match, store it.
+                            lastYearList.push(lastYear); // Push all the matched lastYear into the lastYearList array.
+                            console.log("Last year found!");
                         }
 
-                        else {
-                            console.log('Not working!');
-                            
+                        else { // If there aren't any matches for either, log this.
+                            console.log('A bill matching the current or the previous year wasn\'t found');
                         }
                     });
-                    this.setState({thisYear: currentYearList, lastYear: lastYearList});
-                    console.log(this.state.thisYear.length);
-                    
+                    this.setState({thisYear: currentYearList, lastYear: lastYearList}); // Store the newly collected data into the state.
                 }, 1000);
                 
         
@@ -110,8 +101,8 @@ class Bills extends Component {
                     <thead>
                         <tr>
                             <th scope="col">Type</th>
-                            <th scope="col">Amount &#163;</th>
-                            <th scope="col">Date <i className="fas fa-calendar-alt"></i></th>
+                            <th scope="col">Amount <span className="green">&#163;</span></th>
+                            <th scope="col">Date <i className="fas fa-calendar-alt green"></i></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -125,7 +116,7 @@ class Bills extends Component {
                 </table>
                 <section id="section">
                     <div id = "canvas-container">
-                        <canvas id="myChart" width="400" height="400" aria-label = "temperature chart" role = "chart"></canvas>
+                        <canvas id="myGraph" width="400" height="400" aria-label = "bills graph" role = "graph"></canvas>
                     </div>
                 </section>
             </div>
